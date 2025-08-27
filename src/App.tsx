39134bf30259a5b2364f72ebf267e8638307a6d6
Yudi <yudiyamada10@gmail.com>
2025-08-27 20:59:19 -0300

    inicial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Task from "./components/Tasks";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Task />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Tasks() {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+  return (
+    <>
+    </>
+  )
+}
+
+export default Tasks;
