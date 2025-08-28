6dbf5f118241a721b306758dec575be2647992eb
Yudi <yudiyamada10@gmail.com>
2025-08-27 21:41:25 -0300

    chore: add prettier and eslint config

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -3,10 +3,7 @@ import { useState } from "react";
 function Tasks() {
   const [inputValue, setInputValue] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
-  return (
-    <>
-    </>
-  )
+  return <></>;
 }
 
 export default Tasks;
