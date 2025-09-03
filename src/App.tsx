491c4ab290897f640b671430e292f140a55682bb
Yudi <yudiyamada10@gmail.com>
2025-09-02 22:13:09 -0300

    feat: add Sidebar and SidebarButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <>
-      <h1 className="mb-1.5 bg-red-500 shadow-2xs">Task Manager</h1>
+      <Sidebar />
     </>
   );
 }
---
+++ src/components/SidebarButton.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode } from "react";
+
+type SidebarButtonProps = {
+  children: ReactNode;
+  variant: ReactNode;
+};
+
+const SidebarButton = ({ children, variant }: SidebarButtonProps) => {
+  const getVariantClasses = () => {
+    if (variant === "unselected") {
+      return "text-[#35383E]";
+    }
+
+    if (variant === "selected") {
+      return "bg-[#E6F7F8] text-[#00ADB5]";
+    }
+  };
+
+  return (
+    <a href="/home" className={`px-6 py-3 ${getVariantClasses()}`}>
+      {children}
+    </a>
+  );
+};
+
+export default SidebarButton;
