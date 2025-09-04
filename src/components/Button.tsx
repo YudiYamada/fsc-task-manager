250d971949112aec1183190056e6b70beb2c4933
Yudi <yudiyamada10@gmail.com>
2025-09-04 20:27:43 -0300

    feat: add initial task and button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+type ButtonProps = {
+  children: React.ReactNode;
+  variant: "primary" | "ghost";
+};
+
+const Button = ({ children, variant = "primary" }: ButtonProps) => {
+  const getVariantClasses = () => {
+    if (variant === "primary") {
+      return "bg-[#00ADB5] text-white";
+    }
+
+    if (variant === "ghost") {
+      return "bg-transparent text-[#818181]";
+    }
+  };
+
+  return (
+    <button
+      className={`flex cursor-pointer items-center gap-2 rounded-md px-3 py-1 text-xs transition hover:opacity-70 ${getVariantClasses()}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import Button from "./Button";
+import AddIcon from "../assets/icons/add.svg?react";
+import TrashIcon from "../assets/icons/trash-2.svg?react";
 
-function Tasks() {
-  const [inputValue, setInputValue] = useState("");
-  const [messages, setMessages] = useState<string[]>([]);
-  return <></>;
-}
+const Tasks = () => {
+  return (
+    <div className="w-full px-8 py-16">
+      <div className="flex w-full justify-between">
+        <div>
+          <span className="text-xs font-semibold text-[#00ADB5]">
+            Minhas Tarefas
+          </span>
+          <h2 className="text-xl font-semibold">Minhas Tarefas</h2>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button variant="ghost">
+            <TrashIcon />
+            Limpar tarefas
+          </Button>
+          <Button variant="primary">
+            <AddIcon />
+            Nova tarefa
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Tasks;
