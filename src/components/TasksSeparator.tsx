c9550dda4376182ef8b276364b57c7158675fc1f
Yudi <yudiyamada10@gmail.com>
2025-09-05 17:53:09 -0300

    refactor: add TasksSeparator component

---
+++ src/components/TasksSeparator.tsx
@@ -0,0 +1,15 @@
+type TasksSeparatorProps = {
+  title: React.ReactNode;
+  icon?: React.ReactNode;
+};
+
+const TasksSeparator = ({ title, icon }: TasksSeparatorProps) => {
+  return (
+    <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
+      {icon}
+      <p className="text-sm text-[#9A9C9F]">{title}</p>
+    </div>
+  );
+};
+
+export default TasksSeparator;
