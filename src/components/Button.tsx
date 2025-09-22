425103946bd7e2f7456c2d465b025a9997475c85
Yudi <yudiyamada10@gmail.com>
2025-09-22 17:06:30 -0300

    feat: add size and secondary variant to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,25 @@
 type ButtonProps = {
   children: React.ReactNode;
-  variant: "primary" | "ghost";
+  variant?: "primary" | "secondary" | "ghost";
   onClick?: () => void;
+  size?: "small" | "large";
+  className?: string;
 };
 
-const Button = ({ children, variant = "primary", onClick }: ButtonProps) => {
+const Button = ({
+  children,
+  variant = "primary",
+  onClick,
+  size = "large",
+  className,
+}: ButtonProps) => {
   const getVariantClasses = () => {
     if (variant === "primary") {
       return "bg-[#00ADB5] text-white";
+    }
+
+    if (variant === "secondary") {
+      return "bg-[#EEEEEE] text-[#35383E]";
     }
 
     if (variant === "ghost") {
@@ -15,9 +27,19 @@ const Button = ({ children, variant = "primary", onClick }: ButtonProps) => {
     }
   };
 
+  const getSizeClasses = () => {
+    if (size === "small") {
+      return "py-1 text-xs";
+    }
+
+    if (size === "large") {
+      return "py-2 text-sm";
+    }
+  };
+
   return (
     <button
-      className={`flex cursor-pointer items-center gap-2 rounded-md px-3 py-1 text-xs transition hover:opacity-70 ${getVariantClasses()}`}
+      className={`flex cursor-pointer items-center justify-center gap-2 rounded-md px-3 py-1 text-xs transition hover:opacity-70 ${getVariantClasses()} ${getSizeClasses()} ${className}`}
       onClick={onClick}
     >
       {children}
