50c00e4410ae4b6ba543889b1a5af4a8a23a312e
Yudi <yudiyamada10@gmail.com>
2025-09-23 16:54:21 -0300

    feat: add transition to AddTaskDialog

--- src/components/AddTaskDialogue.tsx
+++ src/components/AddTaskDialogue.tsx
@@ -1,5 +1,9 @@
+import "./AddTaskDialogue.css";
+
 import type { ReactNode } from "react";
+import { useRef } from "react";
 import { createPortal } from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import Button from "./Button";
 import Input from "./Input";
@@ -14,46 +18,64 @@ const AddTaskDialogue = ({
   children,
   handleClose,
 }: AddTaskDialogueProps) => {
-  if (!isOpen) return null;
-  return createPortal(
-    <div className="fixed top-0 bottom-0 left-0 flex h-screen w-screen items-center justify-center backdrop-blur">
-      {/* DIALOG */}
-      <div className="rounded-xl bg-white p-5 text-center shadow-lg">
-        <h2 className="text-xl font-semibold text-[#35383E]">Nova Tarefa</h2>
-        <p className="mt-1 mb-4 text-sm text-[#9A9C9F]">
-          Insira as informações abaixo
-        </p>
-        {children}
+  const nodeRef = useRef(null);
 
-        <div className="flex w-[336px] flex-col space-y-4">
-          <Input
-            id="title"
-            label="Título"
-            placeholder="Insira o título da terefa"
-          />
-          <Input id="time" label="Horário" placeholder="Horário" />
-          <Input
-            id="description"
-            label="Descrição"
-            placeholder="Descreva a tarefa"
-          />
-          <div className="flex gap-3">
-            <Button
-              variant="secondary"
-              size="large"
-              className="w-full"
-              onClick={handleClose}
-            >
-              Cancelar
-            </Button>
-            <Button size="large" className="w-full">
-              Salvar
-            </Button>
-          </div>
-        </div>
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={isOpen}
+      timeout={500}
+      classNames="add-task-dialog"
+      unmountOnExit
+    >
+      <div>
+        {createPortal(
+          <div
+            ref={nodeRef}
+            className="fixed top-0 bottom-0 left-0 flex h-screen w-screen items-center justify-center backdrop-blur"
+          >
+            {/* DIALOG */}
+            <div className="rounded-xl bg-white p-5 text-center shadow-lg">
+              <h2 className="text-xl font-semibold text-[#35383E]">
+                Nova Tarefa
+              </h2>
+              <p className="mt-1 mb-4 text-sm text-[#9A9C9F]">
+                Insira as informações abaixo
+              </p>
+              {children}
+
+              <div className="flex w-[336px] flex-col space-y-4">
+                <Input
+                  id="title"
+                  label="Título"
+                  placeholder="Insira o título da terefa"
+                />
+                <Input id="time" label="Horário" placeholder="Horário" />
+                <Input
+                  id="description"
+                  label="Descrição"
+                  placeholder="Descreva a tarefa"
+                />
+                <div className="flex gap-3">
+                  <Button
+                    variant="secondary"
+                    size="large"
+                    className="w-full"
+                    onClick={handleClose}
+                  >
+                    Cancelar
+                  </Button>
+                  <Button size="large" className="w-full">
+                    Salvar
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>,
+          document.body
+        )}
       </div>
-    </div>,
-    document.body
+    </CSSTransition>
   );
 };
 
