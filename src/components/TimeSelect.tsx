d66df57a81350451130db9525550c42de43db5eb
Yudi <yudiyamada10@gmail.com>
2025-10-07 16:35:03 -0300

    feat: add forwardRef support to TimeSelect component

--- src/components/TimeSelect.tsx
+++ src/components/TimeSelect.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from "react";
+
 import InputErrorMessage from "./InputErrorMessage";
 import InputLabel from "./InputLabel";
 
@@ -5,25 +7,29 @@ type TimeSelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
   errorMessage?: string;
 };
 
-const TimeSelect: React.FC<TimeSelectProps> = (props) => {
-  return (
-    <div className="flex flex-col gap-1 text-left">
-      <InputLabel htmlFor="time">Horário</InputLabel>
+const TimeSelect = forwardRef<HTMLSelectElement, TimeSelectProps>(
+  (props, ref) => {
+    return (
+      <div className="flex flex-col gap-1 text-left">
+        <InputLabel htmlFor="time">Horário</InputLabel>
 
-      <select
-        id="time"
-        className="outline-brand-primary focus:border-brand-primary border-brand-border rounded-lg border border-solid px-4 py-3 placeholder:text-sm"
-        {...props}
-      >
-        <option value="morning">Manhã</option>
-        <option value="afternoon">Tarde</option>
-        <option value="evening">Noite</option>
-      </select>
-      {props.errorMessage && (
-        <InputErrorMessage>{props.errorMessage}</InputErrorMessage>
-      )}
-    </div>
-  );
-};
+        <select
+          id="time"
+          ref={ref}
+          className="outline-brand-primary focus:border-brand-primary border-brand-border rounded-lg border border-solid px-4 py-3 placeholder:text-sm"
+          {...props}
+        >
+          <option value="morning">Manhã</option>
+          <option value="afternoon">Tarde</option>
+          <option value="evening">Noite</option>
+        </select>
+
+        {props.errorMessage && (
+          <InputErrorMessage>{props.errorMessage}</InputErrorMessage>
+        )}
+      </div>
+    );
+  }
+);
 
 export default TimeSelect;
